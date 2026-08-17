8dc4faea79ed9cd827c9a5e6ffdaa0c779eba79a
Emil Piórkowski <e.piorkowski11@gmail.com>
2026-08-17 18:24:02 +0200

    Finish ExpenseViewModel

---
+++ Monevo/Scenes/ExpenseView/ExpenseViewModel.swift
@@ -0,0 +1,82 @@
+//
+//  ExpenseViewModel.swift
+//  Monevo
+//
+//  Created by Emil Piórkowski on 17/08/2026.
+//
+
+import Foundation
+import CoreData
+
+struct CategoryTab: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
+final class ExpenseViewModel {
+    var title = ""
+    var amount = ""
+    var note = ""
+    private(set) var categories: [CategoryTab] = [
+        CategoryTab(title: "Wydatki bieżące"),
+        CategoryTab(title: "Rozrywka i wypoczynek"),
+        CategoryTab(title: "Transport"),
+        CategoryTab(title: "Rachunki"),
+        CategoryTab(title: "Odzież i dodatki"),
+        CategoryTab(title: "Dom"),
+        CategoryTab(title: "Zdrowie"),
+        CategoryTab(title: "Edukacja"),
+        CategoryTab(title: "Wydatki inne")
+    ]
+
+    var selectedCategory: CategoryTab?
+    private(set) var expenses: [Expense] = []
+    private(set) var error: String?
+    
+    let context = PersistenceController.shared.container.viewContext
+    
+    func fetchExpense() {
+        let request = Expense.fetchRequest()
+        
+        do {
+            expenses = try context.fetch(request)
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+    
+    func save() {
+        do {
+            let expense = Expense()
+            expense.id = UUID()
+            expense.title = title
+            expense.note = note
+            expense.category = selectedCategory?.title
+            expense.amount = Double(amount) ?? 0.00
+            
+            try context.save()
+            clear()
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+    
+    func delete(_ expense: Expense?) {
+        do {
+            guard let expense else { return }
+            
+            context.delete(expense)
+            try context.save()
+            fetchExpense()
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+    
+    func clear() {
+        title = ""
+        amount = ""
+        note = ""
+    }
+
+}
